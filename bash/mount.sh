37bf845d2ba6c30ede96808c5ebae4df5180f7d7
Alysson de Sousa RIbeiro <alyssondsr@github.com>
2016-11-24 21:46:13 -0200

    mount vbox

--- bash/mount.sh
+++ bash/mount.sh
@@ -1,3 +1,5 @@
  #!/bin/bash
 sudo chown alysson -R /home/alysson/Downloads/ 
-sudo chown alysson -R /home/alysson/Dropbox/ 
+sudo chown alysson -R /home/alysson/Dropbox/
+sudo mount -t vboxsf Downloads /home/alysson/Downloads/ 
+sudo mount -t vboxsf Dropbox /home/alysson/Dropbox/ 
